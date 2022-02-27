a1aec3f8e0e391ce4d3776fc8f0c9ab24fde8ecc
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2022-02-27 15:40:53 -0300

    generics-set-map example

---
+++ src/application/Program.java
@@ -0,0 +1,30 @@
+package application;
+
+import java.util.Scanner;
+
+import service.PrintService;
+
+public class Program {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		PrintService<Integer> ps = new PrintService<Integer>();
+		
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		//Com type safety não irá permitir passar objeto diferete do tipo
+		//ps.addValue("Mary");
+		
+		for(int i = 0; i < n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		ps.print();
+		Integer x = ps.first();
+		System.out.println("First: " + x );
+		
+		
+	}
+}
